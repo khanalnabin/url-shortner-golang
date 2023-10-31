d4cc14875ee2946dd3c8a3c5eabb67780028a12c
Nabin Khanal <nabinkhanal@skiff.com>
2023-10-31 19:11:55 +0545

    setup routes and dummy handlers

--- handlers.go
+++ handlers.go
@@ -1,1 +1,15 @@
 package main
+
+import "github.com/gofiber/fiber/v2"
+
+func redirectURL(c *fiber.Ctx) error {
+	return nil
+}
+
+func generateURL(c *fiber.Ctx) error {
+	return nil
+}
+
+func indexPage(c *fiber.Ctx) error {
+	return c.Render("index", fiber.Map{})
+}
--- routes.go
+++ routes.go
@@ -3,5 +3,7 @@ package main
 import "github.com/gofiber/fiber/v2"
 
 func setupRoutes(app *fiber.App) {
-
+	app.Get("/:url", redirectURL)
+	app.Post("/", generateURL)
+	app.Get("/", indexPage)
 }
